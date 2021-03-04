839dac41be73cd3cb2202a0622e64b602409edaa
alekseyvy <alekseyvy@gmail.com>
2021-03-04 23:37:35 +0400

    feature: added styled support and home component

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+//Favicons
+export const faIcon = import('./images/icon-facebook.svg');
+export const pinIcon = import('./images/icon-pinterest.svg');
+export const instIcon = import('./images/icon-instagram.svg');
+
+//background assets
+export const bgStars = import('./images/bg-stars.svg');
+export const bgHills = import('./images/pattern-hills.svg');
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <>
+      Here comes Boom!
+    </>
+  )
+}
+
+
+export default Home;
