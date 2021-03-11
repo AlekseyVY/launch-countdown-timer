07973c8083c8351d63a18aed9220e0dbea3cfa34
alekseyvy <alekseyvy@gmail.com>
2021-03-11 18:53:16 +0400

    feature: finished timer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,42 @@
-import {Hills, MainContainer, StarContainer} from "./styles";
+import {Content, HeaderText, Hills, MainContainer, SocialContainer, SocialLink, StarContainer} from "./styles";
 import bgStars from '../../assets/images/bg-stars.svg'
 import hills from '../../assets/images/pattern-hills.svg'
+import FlipCountdown from '@rumess/react-flip-countdown';
+import facebookIcon from '../../assets/images/icon-facebook.svg';
+import pinterestIcon from '../../assets/images/icon-pinterest.svg';
+import instagramIcon from '../../assets/images/icon-instagram.svg';
 
 const Home = () => {
+
+
   return (
     <MainContainer>
       <StarContainer bg={bgStars}>
-
-
-
+        <Content>
+          <HeaderText> WE'RE LAUNCHING SOON</HeaderText>
+          <FlipCountdown
+            size='large'
+            titlePosition='bottom'
+            hideYear
+            hideMonth
+            dayTitle='Days'
+            hourTitle='Hours'
+            minuteTitle='Minutes'
+            secondTitle='Seconds'
+            endAt={'2022-12-12 01:26:58'} // Date/Time
+          />
+          <SocialContainer>
+            <SocialLink>
+              <img src={facebookIcon} alt={'facebook icon'}/>
+            </SocialLink>
+            <SocialLink>
+              <img src={pinterestIcon} alt={'pinterest icon'}/>
+            </SocialLink>
+            <SocialLink>
+              <img src={instagramIcon} alt={'instagram icon'} />
+            </SocialLink>
+          </SocialContainer>
+        </Content>
         <Hills hills={hills}/>
       </StarContainer>
     </MainContainer>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -15,6 +15,9 @@ export const StarContainer = styled.div`
   min-height: calc(100vh - 3px);
   background-image: url(${props => props.bg});
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 export const Hills = styled.div`
@@ -24,4 +27,36 @@ export const Hills = styled.div`
   position: absolute;
   left: 0;
   bottom: -10%;
+`
+
+export const Content = styled.div`
+  display: flex;
+  min-height: 99vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`
+
+export const HeaderText = styled.h1`
+  color: white;
+  font-weight: bold;
+  letter-spacing: 0.75rem;
+  font-size: 2.2rem;
+  @media(max-width: 42.5rem) {
+    font-size: 1.8rem;
+    letter-spacing: 0.614rem;
+  }
+`
+
+export const SocialContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 13.6rem;
+  z-index: 10;
+`
+
+export const SocialLink  = styled.div`
+  &:hover{
+    cursor: pointer;
+  }
 `
